03e1772daa2ac75ef564bf150f7921734402e856
phgeraldeli@id.uff.br <silvaesthermattos.hotmail.com>
2019-09-03 08:05:31 -0300

    correcao das threads e do for com csum

--- PragmaCondicional/PragmaCondicional.c
+++ PragmaCondicional/PragmaCondicional.c
@@ -3,25 +3,26 @@
 #include <string.h>
 #include <omp.h>
 
-#define N 10000
+#define N 10
 
 int main() 
 {
 	float a[N], b[N];
 	int i,n;
 	float temp,csum;
-	n = 10;
+	n = N;
 	csum = 0.0;
 
 	for(i = 0; i< n ; i++) {
 		a[i] = 1;
 		b[i] = 1;
 	}
-
+	omp_set_num_threads(4);
 	#pragma omp parallel shared(a,b,n) private(temp,i) reduction(+:csum)
 	{
+		#pragma omp for
 		for(i=0; i<n ; i++){
-			temp = a[i] / b[i];
+			temp = a[i] + b[i];
 		        csum += temp + 1;	
 		}
 	}
