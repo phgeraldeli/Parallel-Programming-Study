77946cf8dc273ed134164157f6a1c087968d9843
Live session user <renato_bastos@id.uff.br>
2019-09-24 03:04:53 +0000

    Entrega Trabalho 1

---
+++ Trabalho01/MPI_OpenMP.c
@@ -0,0 +1,80 @@
+/*
+ * 
+ * mpicc MPI_OpenMP.c -o MPI_OpenMP -fopenmp
+ * mpirun -np 2 MPI_OpenMP
+ * 
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <string.h>
+#include <time.h>
+
+#define SIZE 100
+int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
+
+int main(int argc, char** argv)
+{
+	int meu_rank, np, origem, destino, tag = 0, master=0;
+	int i,j,k,N;
+	N = SIZE;
+	MPI_Status status;
+	MPI_Init(&argc,&argv);
+	MPI_Comm_rank(MPI_COMM_WORLD,&meu_rank);
+	MPI_Comm_size(MPI_COMM_WORLD,&np);
+
+    clock_t start = clock(); 
+
+	for(i = 0 ; i < SIZE; i++){
+		for(j = 0; j < SIZE; j++) {
+			A[i][j] = 2;
+			B[i][j] = 2;
+			C[i][j] = 0;
+		}
+	}
+
+	printf("-------------- Matriz inicializada --------------\n");
+    omp_set_dynamic(0);
+	omp_set_num_threads(2);
+
+	#pragma omp parallel shared(A,B,C,N) private(i,j,k)
+	#pragma omp for 
+		for(i = meu_rank; i < N; i+=np){
+			for(j = 0; j < N; j++){
+				for(k = 0; k < N; k++){
+				C[i][j] = C[i][j] + A[i][k] * A[k][j];
+			}
+		}
+	}
+	
+	if(meu_rank != master) {
+		for(i=meu_rank;i<N;i+=np){
+			MPI_Send(C[i], SIZE, MPI_INT, master, tag, MPI_COMM_WORLD);
+		}
+	}else {
+		for(origem = 1; origem<np; origem++) {
+			for(j = origem;j < N;j += np){
+				MPI_Recv(C[j], SIZE, MPI_INT, origem, tag, MPI_COMM_WORLD, &status);
+			}
+		}
+
+    printf("\n");
+	for(i = 0; i < N; i++){
+		for(j = 0; j < N; j++){
+			printf("%d ", C[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+    	clock_t end = clock(); 
+    float seconds = (float)(end - start)/CLOCKS_PER_SEC;
+    printf("Durou %f segundos\n", seconds);
+	}
+    
+		
+	MPI_Finalize();
+	return 0;
+}
---
+++ Trabalho01/divisaoThreads.c
@@ -0,0 +1,82 @@
+/*
+ * 
+ *  gcc divisaoThreads.c -o divisaoThreads -fopenmp
+ * ./divisaoThreads.c
+ *	
+ */
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <string.h>
+#include <time.h>
+
+#define SIZE 5
+int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
+
+int main()
+{
+	int i,j,k,N;
+	N = SIZE;
+	omp_set_dynamic(0);
+	omp_set_num_threads(4);
+
+
+	clock_t start = clock();
+
+
+	// Inicializa Matriz com 1
+	#pragma omp parallel private(i,j)
+	#pragma omp for
+	for(i = 0 ; i < SIZE; i++){
+		for(j = 0; j < SIZE; j++) {
+			A[i][j] = 1;
+			B[i][j] = 1;
+			C[i][j] = 1;
+		}
+	}
+
+	printf("-------------- Matriz inicializada --------------\n");
+
+	// Printa a matriz
+        for(i = 0; i < SIZE; i++) {
+                for(j = 0; j < SIZE; j++) {
+                        printf("%d ", C[i][j]);
+                }
+                printf("\n");
+        }
+
+	//Paralelizando Multiplicacao de matrizes
+	#pragma omp parallel shared(A,B,C,N) private(i,j,k)
+	#pragma omp for 
+	
+	for(i = 0; i < N; i++){
+		for( j = 0; j < N; j++) {
+			C[i][j] = 0.0;
+			for (k = 0; k < N ; k++) {
+				C[i][j] = C[i][j] + A[i][k] * B[k][j];
+			}
+		}
+		printf("\nNumero de threads!! %d\n",omp_get_num_threads() );
+	}
+	 printf("-------------- Multiplicacao de matriz --------------\n");
+	// Printa a matriz
+	for(i = 0; i < SIZE; i++) {
+		for(j = 0; j < SIZE; j++) {
+			printf("%d ", C[i][j]);
+		}
+		printf("\n");
+	}
+	clock_t end = clock();
+	float seconds = (float)(end - start)/CLOCKS_PER_SEC;
+    printf("Durou %f segundos\n", seconds);
+	printf("%d",omp_get_num_threads() );
+
+}
+
+
+
+
