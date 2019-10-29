02481f9b1774ee0302204c55b36ae8a99845f60d
phgeraldeli <phgeraldeli@id.uff.br>
2019-10-29 07:28:53 -0300

    Arrumando OpenMP e adicionando clock()

--- Integral/integralOpenMP.c
+++ Integral/integralOpenMP.c
@@ -1,4 +1,9 @@
-main(int argc, char** argv) {
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int argc, char** argv) {
+    
     int my_rank;
     int p; // número de processos
     float a=0.0, b=1.0; // intervalo a calcular
@@ -15,24 +20,25 @@ main(int argc, char** argv) {
 
     float calcula(float local_a, float local_b,int local_n, float h);
     float f(float x);
+    clock_t start = clock();
     omp_set_num_threads(4);
 
-      h = (b-a) / n;
-//    local_n = n / p;
-//    local_a = a + my_rank * local_n * h;
-//    local_b = local_a + local_n * h;
+    h = (b-a) / n;
+    
     integral = 0.0;
-    //integral = ( f(a) + f(b) ) /2.0;
     
     int x=a;
     #pragma omp parallel for reduction(+:integral)
-    for(i=1;i<=n;i++){
+    for(i=1;i < n;i++){
         integral += (i/(double)n)*(i/(double)n);
     }
     integral = integral * h;
-    //integral = integral * h;    
+    integral += 1.0/(2.0*n);
     printf("Resultado: %f\n", integral);
 
+    clock_t end = clock(); 
+    float seconds = (float)(end - start);
+    printf("Durou %f segundos\n", seconds);
 }
 
 float calcula(float local_a, float local_b,int local_n, float h) {
