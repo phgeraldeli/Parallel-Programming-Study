dcfb220b1308b2dc8ec0062fee9109b8ccdd6d55
phgeraldeli <phgeraldeli@id.uff.br>
2019-10-28 23:00:26 -0300

    IntegralMPI Funcionando

--- Integral/integralMPI.c
+++ Integral/integralMPI.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <mpi.h>
-
-main(int argc, char** argv) {
+#include <mpi.h> //usando a versão 4.0.1 do openmpi
+void main(int argc, char** argv) {
     int my_rank;
     int p; // número de processos
     float a=0.0, b=1.0; // intervalo a calcular
@@ -11,49 +10,48 @@ main(int argc, char** argv) {
     int local_n; // número de trapezóides local
     float integral; // integral no meu intervalo
     float total; // integral total
-    int source; // remetente da integral
+    int source; // remetente da integral 
     int dest=0; // destino das integrais (nó 0)
     int tag=200; // tipo de mensagem (único)
-    int resto;
     MPI_Status status;
-
-    float calcula(float local_a, float local_b,
-    int local_n, float h);
-
+    float calcula(float local_a, float local_b, int local_n, float h);
+    int resto;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
-    resto = n / p;
+
+
     h = (b-a) / n;
     local_n = n / p;
-    local_a = a + my_rank * local_n * h;
-    local_b = local_a + local_n * h;
-
+    resto = n%p;
     if(my_rank < resto){
         local_n++;
-        local_a = local_a + my_rank * local_n * h ;
+        local_a = a + my_rank * local_n * h;
     }
     else{
         local_a = a + (resto * (local_n+1) * h) + (my_rank - resto) * local_n * h;
     }
     local_b = local_a + local_n * h;
+
     integral = calcula(local_a, local_b, local_n, h);
 
-    MPI_Reduce(&integral,&total,1,MPI_FLOAT, MPI_SUM,0,MPI_COMM_WORLD);
-
+    MPI_Reduce(&integral, &total, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
     if(my_rank == 0) printf("Resultado: %f\n", total);
     MPI_Finalize();
 }
 
 
-
-float calcula(float local_a, float local_b,int local_n, float h) {
+float calcula(float local_a, float local_b,
+    int local_n, float h) {
     float integral;
     float x, i;
     float f(float x); // função a integrar
+    
     integral = ( f(local_a) + f(local_b) ) /2.0;
+
     x = local_a;
-    for( i=1; i<=local_n; i++) {
+
+    for( i=1; i < local_n; i++) {
         x += h;
         integral += f(x);
     }
@@ -61,8 +59,8 @@ float calcula(float local_a, float local_b,int local_n, float h) {
     return integral;
 }
 
-float f(float x) {
 
+float f(float x) {
     float fx; // valor de retorno
 
     // esta é a função a integrar
@@ -71,4 +69,3 @@ float f(float x) {
 
     return fx;
 }
-
